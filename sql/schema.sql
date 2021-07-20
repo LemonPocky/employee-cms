d64c4ddd70390e00da3cdea1ba156db8e6c55ba3
LemonPocky <someone-70@live.com>
2021-07-19 18:44:26 -0700

    Added table schema file

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE employee_cms_db;
+
+USE employee_cms_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(15, 2) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
